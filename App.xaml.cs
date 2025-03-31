1d5e8cace7c279fbe57fc45585cac0451a3adeae
Kacper Tessmer <kacper.tess@protonmail.com>
2025-03-31 05:43:24 +0200

    Pierwszy commit

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PolMedUMG;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ View/LoginScreen.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PolMedUMG.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnMinimize_Close(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_FullScreen(object sender, RoutedEventArgs e)
+        {
+            if (WindowState != WindowState.Maximized) WindowState = WindowState.Maximized;
+            else WindowState = WindowState.Normal;
+        }
+
+        private void btnLog_In(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_pass_reset(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_acc_create(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
