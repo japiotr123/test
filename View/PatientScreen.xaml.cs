b66f67aa1e77b63153046093e4f1ecd2c2c0c925
Raul Wierzzbiński <Wierzba@tuta.io>
2025-04-22 16:00:16 +0200

    Wprowadzenie funkcjonalnosci dla nawigacji
    Lewa stronu glownego widoku aplikacji (nawigacja) zmienia teraz
    zawartosc prawej strony glownego widoku zgodnie z wybrana opcja.

---
+++ View/PatientScreen.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace PolMedUMG.View
+{
+    public partial class PatientScreen : Window
+    {
+        public PatientScreen()
+        {
+            InitializeComponent();
+            LoadContent(new NewsView());
+        }
+
+        public void LoadContent(UserControl control)
+        {
+
+            if (RightContentPanel != null)
+            {
+                RightContentPanel.Children.Clear();
+                RightContentPanel.Children.Add(control);
+            }
+        }
+
+        private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (NavList.SelectedItem is ListBoxItem selectedItem)
+            {
+                string selectedText = selectedItem.Content.ToString();
+
+                switch (selectedText)
+                {
+                    case "Strona główna":
+                        LoadContent(new NewsView());
+                        break;
+                    case "Umów wizytę":
+                        LoadContent(new MakeAppointment());
+                        break;
+                    case "Kalendarz":
+                        LoadContent(new Calendar());
+                        break;
+                    case "Historia wizyt":
+                        LoadContent(new Visits());
+                        break;
+                    case "Wyniki badań":
+                        LoadContent(new Results());
+                        break;
+                    case "Cennik":
+                        LoadContent(new Pricing());
+                        break;
+                    case "Specjaliści":
+                        LoadContent(new Specialists());
+                        break;
+                    case "Wiadomości":
+                        LoadContent(new Messages());
+                        break;
+                    case "Kontakt":
+                        LoadContent(new Contact());
+                        break;
+                    case "Ustawienia konta":
+                        LoadContent(new Settings());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
