55b513ad2bb1240ac3b2de4c7297f6c90c22e794
Raul Wierzzbiński <Wierzba@tuta.io>
2025-05-26 21:17:11 +0200

    Od teraz doktorzy pobierani sa z bazy danych

--- ViewModel/SpecialistsViewModel.cs
+++ ViewModel/SpecialistsViewModel.cs
@@ -1,34 +1,65 @@
-﻿using PolMedUMG.Model;
+﻿using MySql.Data.MySqlClient;
+using PolMedUMG.Model;
+using PolMedUMG.View;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace PolMedUMG.ViewModel
 {
+    public class Doctor
+    {
+        public string uid { get; set; }
+        public string firstName { get; set; }
+        public string secondName { get; set; }
+
+        public Doctor(string uid, string firstName, string secondName)
+        {
+            this.uid = uid;
+            this.firstName = firstName;
+            this.secondName = secondName;
+        }
+    }
     public class SpecialistsViewModel
     {
         public ObservableCollection<Specialist> Specialists { get; set; }
 
         public SpecialistsViewModel()
         {
-            Specialists = new ObservableCollection<Specialist>
+            Specialists = new ObservableCollection<Specialist>{};
+
+
+            using (MySqlConnection conn = new MySqlConnection(SessionManager.connStrSQL))
             {
-                new Specialist { Icon = "Stethoscope", Title = "Lekarz rodzinny", Name = "dr Anna Nowak" },
-                new Specialist { Icon = "Heartbeat", Title = "Kardiolog", Name = "dr Piotr Zieliński" },
-                new Specialist { Icon = "Shield", Title = "Dermatolog", Name = "dr Marta Kaczmarek" },
-                new Specialist { Icon = "Home", Title = "Pediatra", Name = "dr Tomasz Wójcik" },
-                new Specialist { Icon = "UserMd", Title = "Dentysta", Name = "dr Agnieszka Pich" },
-                new Specialist { Icon = "UserMd", Title = "Ginekolog", Name = "dr Michał Kamiński" },
-                new Specialist { Icon = "Bed", Title = "Ortopeda", Name = "dr Katarzyna Woźniak" },
-                new Specialist { Icon = "UserMd", Title = "Endokrynolog", Name = "dr Marcin Jankowskio" },
-                new Specialist { Icon = "Eye", Title = "Okulista", Name = "dr Barbara Wiśniewska" },
-                new Specialist { Icon = "UserMd", Title = "Neurolog", Name = "dr Łukasz Kowalczyk" },
-                new Specialist { Icon = "UserMd", Title = "Laryngolog", Name = "dr Ewa Szymańska" },
-                new Specialist { Icon = "UserMd", Title = "Reumatolog", Name = "dr Paweł Mak" }
-            };
+                try
+                {
+                    conn.Open();
+                    string sql = "SELECT `uid`, `firstName`, `secondName` FROM `users` WHERE acc_type=1 LIMIT 12;";
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Doctor doctor = new Doctor(
+                                reader["uid"].ToString(),
+                                reader["firstName"].ToString(),
+                                reader["secondName"].ToString()
+                            );
+
+                            Specialists.Add(new Specialist { Icon = "UserMd", Title = "Tytuł", Name = $"dr. {doctor.firstName} {doctor.secondName}" });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Obsługa błędów
+                    Console.WriteLine("Błąd podczas pobierania danych: " + ex.Message);
+                }
+            }
         }
     }
 }
