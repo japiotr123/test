6390c7c4f3316023bbeee5680ecc1f67356ae9bb
Piotr Kapitkowski <japiotr123@gmail.com>
2025-06-01 22:11:44 +0200

    dodanie cennika, połączenie z bazą danych

--- View/Pricing.xaml.cs
+++ View/Pricing.xaml.cs
@@ -1,27 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MySql.Data.MySqlClient;
+using PolMedUMG.ViewModel;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
 namespace PolMedUMG.View
 {
     /// <summary>
-    /// Logika interakcji dla klasy Pricing.xaml
-    /// </summary>
+   /// Logika interakcji dla klasy Calendar.xaml
+   /// </summary>
     public partial class Pricing : UserControl
     {
         public Pricing()
         {
             InitializeComponent();
+            DataContext = new PricingViewModel  ();
+
         }
     }
 }
---
+++ ViewModel/PricingViewModel.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using PolMedUMG.View;
+using System.Windows;
+
+namespace PolMedUMG.ViewModel
+{
+    public class PricingViewModel
+    {
+        public List<Model.Prices> prices { get; set; }
+
+        public PricingViewModel()
+        {
+            prices = [new Model.Prices { Service_name = "Ładowanie...", Price = "..." }];
+            ;
+            using (MySqlConnection conn = new MySqlConnection(SessionManager.connStrSQL))
+            {
+                try
+                {
+                    conn.Open();
+                    string sql = "SELECT `name`, `price` FROM `Services`";
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    {
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            prices.Clear();
+                            while (reader.Read())
+                            {
+                                prices.Add(new Model.Prices
+                                {
+                                    Service_name = reader["name"].ToString() ?? "",
+                                    Price = reader["price"].ToString() ?? "",
+                                });
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Obsługa błędów
+                    MessageBox.Show("Błąd podczas pobierania danych: " + ex.Message);
+                }
+            }
+
+        }
+
+    }
+}
